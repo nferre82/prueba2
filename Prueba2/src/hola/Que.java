3fcd7ccfbf17b1dd63134b6ceb0355a96a6ab88d
nferre82 <nelsonferre82@gmail.com>
2021-05-20 11:13:43 +0200

    Inicio

---
+++ Prueba2/src/hola/Que.java
@@ -0,0 +1,14 @@
+package hola;
+
+public class Que {
+
+	public Que() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("ase wey");
+
+	}
+
+}
