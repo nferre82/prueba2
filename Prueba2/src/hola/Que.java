515d23cf55f15243fda35d98cd78f57f9513354f
nferre82 <nelsonferre82@gmail.com>
2021-05-20 11:15:25 +0200

    new constructor anyadido

--- Prueba2/src/hola/Que.java
+++ Prueba2/src/hola/Que.java
@@ -5,7 +5,9 @@ public class Que {
 	public Que() {
 		// TODO Auto-generated constructor stub
 	}
-
+	public Que(String str) {
+		System.out.println(str);
+	}
 	public static void main(String[] args) {
 		System.out.println("ase wey");
 
